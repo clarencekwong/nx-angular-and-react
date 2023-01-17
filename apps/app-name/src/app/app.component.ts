5895452737db1f070f3b47b7d51eb1b20ac001a8
Clarence Kwong <clarence.kwong@pcbank.ca>
2023-01-17 14:23:45 -0500

    ang and react added

---
+++ apps/app-name/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'poc-angreact-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'app-name';
+}
